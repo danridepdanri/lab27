0ede46da826652aa1cf5a694874ebc0171c4fddf
danridepdanri <106473655+danridepdanri@users.noreply.github.com>
2023-05-26 20:23:35 +0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drivesListBox = new System.Windows.Forms.ListBox();
+            this.directoriesListBox = new System.Windows.Forms.ListBox();
+            this.filesListBox = new System.Windows.Forms.ListBox();
+            this.propertiesTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.contentTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drivesListBox
+            // 
+            this.drivesListBox.FormattingEnabled = true;
+            this.drivesListBox.Location = new System.Drawing.Point(13, 45);
+            this.drivesListBox.Name = "drivesListBox";
+            this.drivesListBox.Size = new System.Drawing.Size(148, 147);
+            this.drivesListBox.TabIndex = 0;
+            this.drivesListBox.SelectedIndexChanged += new System.EventHandler(this.drivesListBox_SelectedIndexChanged);
+            // 
+            // directoriesListBox
+            // 
+            this.directoriesListBox.FormattingEnabled = true;
+            this.directoriesListBox.Location = new System.Drawing.Point(513, 45);
+            this.directoriesListBox.Name = "directoriesListBox";
+            this.directoriesListBox.Size = new System.Drawing.Size(213, 186);
+            this.directoriesListBox.TabIndex = 1;
+            this.directoriesListBox.SelectedIndexChanged += new System.EventHandler(this.directoriesListBox_SelectedIndexChanged);
+            // 
+            // filesListBox
+            // 
+            this.filesListBox.FormattingEnabled = true;
+            this.filesListBox.Location = new System.Drawing.Point(513, 237);
+            this.filesListBox.Name = "filesListBox";
+            this.filesListBox.Size = new System.Drawing.Size(213, 186);
+            this.filesListBox.TabIndex = 2;
+            this.filesListBox.SelectedIndexChanged += new System.EventHandler(this.filesListBox_SelectedIndexChanged);
+            // 
+            // propertiesTextBox
+            // 
+            this.propertiesTextBox.Location = new System.Drawing.Point(233, 45);
+            this.propertiesTextBox.Multiline = true;
+            this.propertiesTextBox.Name = "propertiesTextBox";
+            this.propertiesTextBox.Size = new System.Drawing.Size(214, 96);
+            this.propertiesTextBox.TabIndex = 3;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 275);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(381, 163);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // contentTextBox
+            // 
+            this.contentTextBox.Location = new System.Drawing.Point(233, 156);
+            this.contentTextBox.Multiline = true;
+            this.contentTextBox.Name = "contentTextBox";
+            this.contentTextBox.Size = new System.Drawing.Size(214, 96);
+            this.contentTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.contentTextBox);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.propertiesTextBox);
+            this.Controls.Add(this.filesListBox);
+            this.Controls.Add(this.directoriesListBox);
+            this.Controls.Add(this.drivesListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox drivesListBox;
+        private System.Windows.Forms.ListBox directoriesListBox;
+        private System.Windows.Forms.ListBox filesListBox;
+        private System.Windows.Forms.TextBox propertiesTextBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox contentTextBox;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            LoadDrives();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                drivesListBox.Items.Add(drive);
+            }
+        }
+
+        private void drivesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (drivesListBox.SelectedItem != null)
+            {
+                string selectedDrive = drivesListBox.SelectedItem.ToString();
+                DisplayDriveProperties(selectedDrive);
+                LoadDirectories(selectedDrive);
+                LoadFiles(selectedDrive);
+            }
+        }
+
+        private void directoriesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedDirectory = directoriesListBox.SelectedItem.ToString();
+            DisplayDirectoryProperties(selectedDirectory);
+            LoadFiles(selectedDirectory);
+        }
+
+        private void filesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedFile = filesListBox.SelectedItem.ToString();
+            DisplayFileProperties(selectedFile);
+            if (IsImageFile(selectedFile))
+            {
+                DisplayImage(selectedFile);
+            }
+            else if (IsTextFile(selectedFile))
+            {
+                DisplayTextFileContent(selectedFile);
+            }
+        }
+
+        private void LoadDirectories(string path)
+        {
+            try
+            {
+                string[] directories = Directory.GetDirectories(path);
+                directoriesListBox.Items.Clear();
+                foreach (string directory in directories)
+                {
+                    directoriesListBox.Items.Add(directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading directories: " + ex.Message);
+            }
+        }
+
+        private void LoadFiles(string path)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+                filesListBox.Items.Clear();
+                foreach (string file in files)
+                {
+                    filesListBox.Items.Add(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading files: " + ex.Message);
+            }
+        }
+
+        private void DisplayDriveProperties(string drive)
+        {
+            DriveInfo driveInfo = new DriveInfo(drive);
+            propertiesTextBox.Text = $"Drive Name: {driveInfo.Name}\r\n" +
+                $"Drive Type: {driveInfo.DriveType}\r\n" +
+                $"Total Size: {driveInfo.TotalSize}\r\n" +
+                $"Available Free Space: {driveInfo.AvailableFreeSpace}\r\n";
+        }
+
+        private void DisplayDirectoryProperties(string directory)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+            propertiesTextBox.Text = $"Directory Name: {directoryInfo.Name}\r\n" +
+                $"Created: {directoryInfo.CreationTime}\r\n" +
+                $"Last Modified: {directoryInfo.LastWriteTime}\r\n" +
+                $"Attributes: {directoryInfo.Attributes}\r\n";
+        }
+
+        private void DisplayFileProperties(string file)
+        {
+            FileInfo fileInfo = new FileInfo(file);
+            propertiesTextBox.Text = $"File Name: {fileInfo.Name}\r\n" +
+                $"Size: {fileInfo.Length}\r\n" +
+                $"Created: {fileInfo.CreationTime}\r\n" +
+                $"Last Modified: {fileInfo.LastWriteTime}\r\n" +
+                $"Attributes: {fileInfo.Attributes}\r\n";
+        }
+
+        private void DisplayImage(string imagePath)
+        {
+            try
+            {
+                pictureBox.Image = Image.FromFile(imagePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error displaying image: " + ex.Message);
+            }
+        }
+
+        private void DisplayTextFileContent(string filePath)
+        {
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                contentTextBox.Text = content;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error displaying text file content: " + ex.Message);
+            }
+        }
+
+        private bool IsImageFile(string fileName)
+        {
+            string extension = Path.GetExtension(fileName);
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+            return Array.Exists(imageExtensions, ext => ext.Equals(extension, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private bool IsTextFile(string fileName)
+        {
+            string extension = Path.GetExtension(fileName);
+            string[] textExtensions = { ".txt", ".csv", ".log" };
+            return Array.Exists(textExtensions, ext => ext.Equals(extension, StringComparison.OrdinalIgnoreCase));
+        }
+
+       
+    }
+}
